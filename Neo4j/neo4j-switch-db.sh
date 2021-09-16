e1d6cd7e0d40870bb320f6a4ee941ff82511c220
Dmitriy "DK" Korobskiy <dkroot2@gmail.com>
2021-09-16 17:18:38 -0400

    ETL of clusters

--- Neo4j/neo4j-switch-db.sh
+++ Neo4j/neo4j-switch-db.sh
@@ -8,22 +8,26 @@ NAME
 
 SYNOPSIS
 
-    neo4j-switch-db.sh neo4j_db
+    neo4j-switch-db.sh [-r] neo4j_db
     neo4j-switch-db.sh -h: display this help
 
 DESCRIPTION
 
-    Switch Neo4j 4 default DB and restart Neo4j. This also would activate a new imported DB.
+    Switch Neo4j 4 default DB.
+
+    The following options are available:
+
+    -r            Restart Neo4j. This also would activate a new default DB.
+                  Executing user must be able to run `systemctl`: either run the script under `root` or enable this
+                  via PolKit.
 
 ENVIRONMENT
-
-    * Executing user must be able to run `systemctl`: either run the script under `root` or enable this via PolKit.
 
     * Neo4j must be set up to permit configuration update for the executing user: either run under `neo4j` or set up
     `neo4j` group permissions:
       * `sudo chmod -R g+w /etc/neo4j`
 
-v2.0                                     August 2021                                   Created by Dmitriy "DK" Korobskiy
+v3.0                                   September 2021                                  Created by Dmitriy "DK" Korobskiy
 HEREDOC
   exit 1
 }
@@ -31,7 +35,21 @@ HEREDOC
 set -e
 set -o pipefail
 
-[[ $1 == "-h" || $# -lt 1 ]] && usage
+# If a character is followed by a colon, the option is expected to have an argument
+while getopts rh OPT; do
+  case "$OPT" in
+    r)
+      readonly RESTART=true
+      ;;
+    *) # -h or `?`: an unknown option
+      usage
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+readonly DB_NAME="$1"
+[[ $DB_NAME == "" ]] && usage
 
 # `${USER:-${USERNAME:-${LOGNAME}}}` might be not available inside Docker containers
 echo -e "\n# neo4j-switch-db.sh: running under $(whoami)@${HOSTNAME} in ${PWD} #\n"
@@ -41,20 +59,23 @@ if ! command -v cypher-shell >/dev/null; then
   exit 1
 fi
 
-readonly DB_NAME="$1"
-
 sed --in-place --expression="s/^dbms.default_database=.*/dbms.default_database=${DB_NAME}/" /etc/neo4j/neo4j.conf
+echo "Set the default database = $DB_NAME"
 
-echo "Restarting Neo4j with a new default database: $DB_NAME ..."
-systemctl restart neo4j
+if [[ $RESTART ]]; then
+  echo "Restarting Neo4j with a new default database..."
+  systemctl restart neo4j
 
-declare -i time_limit_s=30
-echo "Waiting for the service to become active up to ${time_limit_s} seconds ..."
-# Ping Neo4j. Even if a service is active it might not be responding yet.
-while ! cypher-shell "CALL dbms.components()" 2>/dev/null; do
-  if ((time_limit_s-- == 0)); then
-    echo "ERROR: Neo4j with the default DB ${DB_NAME} failed to start." >&2
-    exit 2
-  fi
-  sleep 1
-done
+  declare -i time_limit_s=30
+  echo "Waiting for the service to become active up to ${time_limit_s} seconds ..."
+  # Ping Neo4j. Even if a service is active it might not be responding yet.
+  while ! cypher-shell "CALL dbms.components()" 2>/dev/null; do
+    if ((time_limit_s-- == 0)); then
+      echo "ERROR: Neo4j with the default DB ${DB_NAME} failed to start." >&2
+      exit 2
+    fi
+    sleep 1
+  done
+else
+  echo "WARNING: the new default database will not be activated until the service is restarted"
+fi
