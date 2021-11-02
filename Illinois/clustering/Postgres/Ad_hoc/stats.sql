0cc55c9ac6e8083c2a869fbf1758bd8c61f0aa24
Dmitriy "DK" Korobskiy <dkroot2@gmail.com>
2021-11-01 20:24:00 -0400

    Minor

--- Illinois/clustering/Postgres/Ad_hoc/stats.sql
+++ Illinois/clustering/Postgres/Ad_hoc/stats.sql
@@ -6,4 +6,5 @@ SET TIMEZONE = 'US/Eastern';
 
 SELECT clustering_version, count(1) AS num_clusters
 FROM clusters.exosome_1900_2010_clusters
-GROUP BY clustering_version;
+GROUP BY clustering_version
+ORDER BY clustering_version;
