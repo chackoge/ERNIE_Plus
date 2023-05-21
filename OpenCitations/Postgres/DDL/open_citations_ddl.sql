d66b65552c74bf3439dc561fcce325ff1737c57c
Dima Korobskiy <dkroot2@gmail.com>
2023-05-21 17:46:15 -0400

    Open Citations ETL 2.0

--- OpenCitations/Postgres/DDL/open_citations_ddl.sql
+++ OpenCitations/Postgres/DDL/open_citations_ddl.sql
@@ -5,35 +5,6 @@
 SET search_path = :schema;
 \endif
 
--- JetBrains IDEs: start execution from here
-SET TIMEZONE = 'US/Eastern';
-
-CREATE TABLE cr_publications (
-  doi VARCHAR(400)
-    CONSTRAINT cr_publications_pk
-      PRIMARY KEY /*USING INDEX TABLESPACE index_tbs*/,
-  volume VARCHAR(40),
-  pages VARCHAR(80),
-  issn CHAR(9),
-  deposited_date DATE,
-  published_year SMALLINT
-);
-
-CREATE INDEX IF NOT EXISTS cp_published_year_i ON cr_publications(published_year) /*TABLESPACE index_tbs*/;
-
-CREATE INDEX IF NOT EXISTS cp_issn_i ON cr_publications(issn) /*TABLESPACE index_tbs*/;
-
-CREATE TABLE cr_pub_authors (
-  doi VARCHAR(400),
-  given_name VARCHAR(200),
-  family_name VARCHAR(400),
-  CONSTRAINT cr_pub_authors_pk
-    PRIMARY KEY (doi, family_name, given_name) --USING INDEX TABLESPACE index_tbs
-);
-
-CREATE INDEX IF NOT EXISTS cpa_family_name_given_name_i ON cr_pub_authors(family_name, given_name)
-/*TABLESPACE index_tbs*/;
-
 CREATE TABLE open_citations (
   oci VARCHAR(1000)
     CONSTRAINT open_citations_pk
@@ -42,14 +13,29 @@ CREATE TABLE open_citations (
   cited VARCHAR(400),
   creation_date DATE,
   time_span INTERVAL,
+  journal_sc BOOLEAN,
+  author_sc BOOLEAN,
   citing_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date) ) STORED,
   cited_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date - time_span) ) STORED
 )
 TABLESPACE open_citations_tbs;
 
-CREATE INDEX IF NOT EXISTS oc_citing_i ON open_citations(citing) TABLESPACE open_citations_tbs;
+CREATE UNIQUE INDEX IF NOT EXISTS open_citations_uk ON open_citations(citing, cited) TABLESPACE open_citations_tbs;
 
 CREATE INDEX IF NOT EXISTS oc_cited_i ON open_citations(cited) TABLESPACE open_citations_tbs;
+
+--CREATE INDEX IF NOT EXISTS oc_citing_i ON open_citations(citing) TABLESPACE open_citations_tbs;
+
+--CREATE INDEX IF NOT EXISTS oc_cited_i ON open_citations(cited) TABLESPACE open_citations_tbs;
+
+COMMENT ON TABLE open_citations IS --
+  'Open Citations COCI: Crossref open DOI-to-DOI citations excluding duplicate, parallel, self and loop citations';
+
+COMMENT ON COLUMN open_citations.citing IS --
+  'DOI, lower-cased';
+
+COMMENT ON COLUMN open_citations.cited IS --
+  'DOI, lower-cased';
 
 COMMENT ON COLUMN open_citations.creation_date IS --
   'The date on which the citation was created. This has the same value as the publication date of the citing
@@ -60,42 +46,107 @@ COMMENT ON COLUMN open_citations.time_span IS --
   'The time span of a citation, i.e. the interval between the publication of the citing entity and the publication
 of the cited entity.';
 
+COMMENT ON COLUMN open_citations.journal_sc IS --
+  'Whether it is a journal self-citation (i.e. the citing and the cited entities are published in the same journal)';
+
+COMMENT ON COLUMN open_citations.author_sc IS --
+  'Whether it is an author self-citation (i.e. the citing and the cited entities have at least one author in common).';
+
 ALTER TABLE open_citations
-  OWNER TO developers;
+  OWNER TO devs;
+
+CREATE TABLE open_citation_duplicates (
+  oci VARCHAR(1000),
+  citing VARCHAR(400),
+  cited VARCHAR(400),
+  creation_date DATE,
+  time_span INTERVAL,
+  journal_sc BOOLEAN,
+  author_sc BOOLEAN,
+  citing_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date) ) STORED,
+  cited_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date - time_span) ) STORED
+)
+TABLESPACE open_citations_tbs;
+
+COMMENT ON TABLE open_citation_duplicates IS 'Citations that duplicate an OCI in open_citations';
+
+ALTER TABLE open_citation_duplicates
+  OWNER TO devs;
+
+CREATE TABLE open_citation_parallels (
+  oci VARCHAR(1000)
+    CONSTRAINT open_citation_duplicates_pk
+      PRIMARY KEY USING INDEX TABLESPACE open_citations_tbs,
+  citing VARCHAR(400),
+  cited VARCHAR(400),
+  creation_date DATE,
+  time_span INTERVAL,
+  journal_sc BOOLEAN,
+  author_sc BOOLEAN,
+  citing_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date) ) STORED,
+  cited_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date - time_span) ) STORED
+)
+TABLESPACE open_citations_tbs;
+
+COMMENT ON TABLE open_citation_parallels IS 'Citations that parallel (citing -> cited) in open_citations';
+
+ALTER TABLE open_citation_parallels
+  OWNER TO devs;
+
+CREATE TABLE open_citation_self (
+  oci VARCHAR(1000)
+    CONSTRAINT open_citation_self_pk
+      PRIMARY KEY USING INDEX TABLESPACE open_citations_tbs,
+  citing VARCHAR(400),
+  cited VARCHAR(400),
+  creation_date DATE,
+  time_span INTERVAL,
+  journal_sc BOOLEAN,
+  author_sc BOOLEAN,
+  citing_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date) ) STORED,
+  cited_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date - time_span) ) STORED
+)
+TABLESPACE open_citations_tbs;
+
+COMMENT ON TABLE open_citation_self IS 'Citations with citing = cited';
+
+ALTER TABLE open_citation_self
+  OWNER TO devs;
+
+CREATE TABLE open_citation_loops (
+  oci VARCHAR(1000)
+    CONSTRAINT open_citation_loops_pk
+      PRIMARY KEY USING INDEX TABLESPACE open_citations_tbs,
+  citing VARCHAR(400),
+  cited VARCHAR(400),
+  creation_date DATE,
+  time_span INTERVAL,
+  journal_sc BOOLEAN,
+  author_sc BOOLEAN,
+  citing_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date) ) STORED,
+  cited_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date - time_span) ) STORED
+)
+TABLESPACE open_citations_tbs;
+
+COMMENT ON TABLE open_citation_loops IS 'Citations that loop back (cited -> citing) in open_citations';
+
+ALTER TABLE open_citation_loops
+  OWNER TO devs;
 
 CREATE OR REPLACE VIEW stg_open_citations AS
-SELECT oci, citing, cited, 'foo' AS creation, 'bar' AS timespan, 'baz' AS journal_sc, 'qux' AS author_sc
+SELECT oci, citing, cited, 'foo' AS creation, 'bar' AS timespan, journal_sc, author_sc
   FROM open_citations;
 
 \include_relative trg_transform_and_load_open_citation.sql
 
--- 32h:35m
--- TBD Refactor to a MATERIALIZED VIEW: some weird disk space errors were preventing that.
-CREATE TABLE open_citations_invalid
-TABLESPACE open_citations_tbs AS
-SELECT *
-  FROM open_citations oc
- WHERE
-    -- Self-citations
-   citing = cited
-    OR
-    -- Parallel edges
-   EXISTS(SELECT 1 FROM open_citations oc2 WHERE oc2.citing = oc.citing AND oc2.cited = oc.cited AND oc2.oci <> oc.oci);
-
-COMMENT ON TABLE open_citations_invalid IS ---
-  'Self-citations and parallel edges';
-
-ALTER TABLE open_citations_invalid
-  ADD CONSTRAINT open_citations_invalid_pk
-    PRIMARY KEY (oci) USING INDEX TABLESPACE open_citations_tbs;
-
 CREATE SEQUENCE open_citation_pubs_seq MINVALUE 0;
 
 -- TBD Refactor to a MATERIALIZED VIEW: some weird disk space errors were preventing that.
-CREATE TABLE open_citation_pubs
-TABLESPACE open_citations_tbs AS
+CREATE MATERIALIZED VIEW open_citation_pubs
+  --CREATE TABLE open_citation_pubs
+  TABLESPACE open_citations_tbs AS
 SELECT sq.doi, nextval('open_citation_pubs_seq') AS iid
-  FROM ( -- DOI case can be normalized, e.g. lower-cased, but this yields the same number of publications
+  FROM (
     SELECT citing AS doi
       FROM open_citations
      UNION
@@ -104,13 +155,15 @@ SELECT sq.doi, nextval('open_citation_pubs_seq') AS iid
       FROM open_citations
   ) sq;
 
-COMMENT ON TABLE open_citation_pubs IS ---
+COMMENT ON MATERIALIZED VIEW open_citation_pubs IS ---
   'Unique publications with original DOIs extracted from open_citations';
 
+/*
 ALTER TABLE open_citation_pubs
   ADD CONSTRAINT open_citation_pubs_pk
     PRIMARY KEY (doi) USING INDEX TABLESPACE open_citations_tbs;
 -- 1m:01s–4m:02s
+*/
 
 CREATE UNIQUE INDEX IF NOT EXISTS open_citations_pubs_uk ON open_citation_pubs(iid) --
   TABLESPACE open_citations_tbs;
--- OpenCitations/Postgres/DDL/trg_transform_and_load_open_citation.sql
+++ OpenCitations/Postgres/DDL/trg_transform_and_load_open_citation.sql
@@ -1,31 +1,113 @@
+\set ON_ERROR_STOP on
+\set ECHO all
+
+\if :{?schema}
+SET search_path = :schema;
+\endif
+
+CREATE OR REPLACE FUNCTION to_date(date_string TEXT) --
+  RETURNS DATE
+  /**
+  Converts YYYY[-MM][-DD] string to a DATE
+  @return default omitted month to June and omitted day to 15
+  */
+RETURN make_date(cast(left(date_string, 4) AS INT), --
+                 cast(coalesce(nullif(substr(date_string, 6, 2), ''), '6') AS INT),
+                 cast(coalesce(nullif(substr(date_string, 9), ''), '15') AS INT));
+
+CREATE OR REPLACE FUNCTION to_interval(signed_iso_8601_interval TEXT) --
+  RETURNS INTERVAL
+  /**
+  Converts [-]P{quantity}{unit}[{quantity}{unit} ...] string to an INTERVAL
+  */
+RETURN CASE
+         WHEN left(signed_iso_8601_interval, 1) = '-' THEN -cast(substr(signed_iso_8601_interval, 2) AS INTERVAL)
+         ELSE cast(signed_iso_8601_interval AS INTERVAL)
+       END;
+
 -- trg_transform_and_load_open_citation(): routine
 CREATE OR REPLACE FUNCTION trg_transform_and_load_open_citation() --
   RETURNS TRIGGER
   LANGUAGE plpgsql --
-AS $block$
+AS
+$block$
 BEGIN
   IF (tg_op = 'INSERT') THEN
-    INSERT INTO open_citations(oci, citing, cited, creation_date, time_span)
-    VALUES
-      (new.oci,
-       new.citing,
-       new.cited,
-       make_date(cast(left(new.creation, 4) AS INT), cast(coalesce(nullif(substr(new.creation, 6, 2), ''), '6') AS INT),
-                 cast(coalesce(nullif(substr(new.creation, 9), ''), '15') AS INT)),
-       CASE
-         WHEN left(new.timespan, 1) = '-'
-           THEN -CAST(SUBSTR(new.timespan, 2) AS INTERVAL)
-         ELSE CAST(new.timespan AS INTERVAL)
-       END)
-        ON CONFLICT (oci) DO UPDATE SET citing = excluded.citing,
-          cited = excluded.cited,
-          time_span = excluded.time_span,
-          creation_date = excluded.creation_date;
+    INSERT INTO open_citation_self(oci, citing, cited, creation_date, time_span, author_sc, journal_sc)
+    SELECT new.oci,
+           new.citing,
+           new.cited,
+           to_date(new.creation),
+           to_interval(new.timespan),
+           new.author_sc,
+           new.journal_sc
+    FROM open_citations oc
+    WHERE lower(new.citing) = lower(new.cited);
+    IF FOUND THEN
+      RETURN NULL;
+    END IF;
 
+    INSERT INTO open_citation_duplicates(oci, citing, cited, creation_date, time_span, author_sc, journal_sc)
+    SELECT new.oci,
+           new.citing,
+           new.cited,
+           to_date(new.creation),
+           to_interval(new.timespan),
+           new.author_sc,
+           new.journal_sc
+    FROM open_citations oc
+    WHERE oc.oci = new.oci;
+    IF FOUND THEN
+      RETURN NULL;
+    END IF;
+
+    INSERT INTO open_citation_parallels(oci, citing, cited, creation_date, time_span, author_sc, journal_sc)
+    SELECT new.oci,
+           new.citing,
+           new.cited,
+           to_date(new.creation),
+           to_interval(new.timespan),
+           new.author_sc,
+           new.journal_sc
+    FROM open_citations oc
+    WHERE oc.citing = lower(new.citing)
+      AND oc.cited = lower(new.cited);
+    IF FOUND THEN
+      RETURN NULL;
+    END IF;
+
+    INSERT INTO open_citation_loops(oci, citing, cited, creation_date, time_span, author_sc, journal_sc)
+    SELECT new.oci,
+           new.citing,
+           new.cited,
+           to_date(new.creation),
+           to_interval(new.timespan),
+           new.author_sc,
+           new.journal_sc
+    FROM open_citations oc
+    WHERE oc.citing = lower(new.cited)
+      AND oc.cited = lower(new.citing);
+    IF FOUND THEN
+      RETURN NULL;
+    END IF;
+
+    INSERT INTO open_citations(oci, citing, cited, creation_date, time_span, author_sc, journal_sc)
+    VALUES (new.oci,
+            lower(new.citing),
+            lower(new.cited),
+            to_date(new.creation),
+            to_interval(new.timespan),
+            new.author_sc,
+            new.journal_sc);
+    /*
+    A nonnull return value is used to signal that the trigger performed the necessary data modifications in the view.
+    This will cause the count of the number of rows affected by the command to be incremented
+    */
     RETURN new;
   END IF;
   RAISE 'Operation % is not supported', tg_op;
-END; $block$;
+END;
+$block$;
 
 DROP TRIGGER IF EXISTS stg_open_citations_trg ON stg_open_citations;
 CREATE TRIGGER stg_open_citations_trg
