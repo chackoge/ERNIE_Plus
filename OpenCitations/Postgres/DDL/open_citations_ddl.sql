e6f4c38a79e2007fc6ba109f7d1fd24d7f3752ee
Dima Korobskiy <dkroot2@gmail.com>
2023-05-23 11:52:11 -0400

    Add pre- and post-processing SQL with SET UNLOGGED/LOGGED

--- OpenCitations/Postgres/DDL/open_citations_ddl.sql
+++ OpenCitations/Postgres/DDL/open_citations_ddl.sql
@@ -5,18 +5,19 @@
 SET search_path = :schema;
 \endif
 
-CREATE TABLE open_citations (
-  oci VARCHAR(1000)
+CREATE TABLE open_citations
+(
+  oci             VARCHAR(1000)
     CONSTRAINT open_citations_pk
       PRIMARY KEY USING INDEX TABLESPACE open_citations_tbs,
-  citing VARCHAR(400),
-  cited VARCHAR(400),
-  creation_date DATE,
-  time_span INTERVAL,
-  journal_sc BOOLEAN,
-  author_sc BOOLEAN,
+  citing          VARCHAR(400),
+  cited           VARCHAR(400),
+  creation_date   DATE,
+  time_span       INTERVAL,
+  journal_sc      BOOLEAN,
+  author_sc       BOOLEAN,
   citing_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date) ) STORED,
-  cited_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date - time_span) ) STORED
+  cited_pub_year  SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date - time_span) ) STORED
 ) TABLESPACE open_citations_tbs;
 
 CREATE UNIQUE INDEX IF NOT EXISTS open_citations_uk ON open_citations (citing, cited) TABLESPACE open_citations_tbs;
@@ -54,16 +55,17 @@ COMMENT ON COLUMN open_citations.author_sc IS --
 ALTER TABLE open_citations
   OWNER TO devs;
 
-CREATE TABLE open_citation_duplicates (
-  oci VARCHAR(1000),
-  citing VARCHAR(400),
-  cited VARCHAR(400),
-  creation_date DATE,
-  time_span INTERVAL,
-  journal_sc BOOLEAN,
-  author_sc BOOLEAN,
+CREATE TABLE open_citation_duplicates
+(
+  oci             VARCHAR(1000),
+  citing          VARCHAR(400),
+  cited           VARCHAR(400),
+  creation_date   DATE,
+  time_span       INTERVAL,
+  journal_sc      BOOLEAN,
+  author_sc       BOOLEAN,
   citing_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date) ) STORED,
-  cited_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date - time_span) ) STORED,
+  cited_pub_year  SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date - time_span) ) STORED,
   CONSTRAINT open_citation_duplicates_pk
     PRIMARY KEY (oci, citing, cited, creation_date, time_span, journal_sc,
                  author_sc) USING INDEX TABLESPACE open_citations_tbs
@@ -74,16 +76,17 @@ COMMENT ON TABLE open_citation_duplicates IS 'Citations that duplicate an OCI in
 ALTER TABLE open_citation_duplicates
   OWNER TO devs;
 
-CREATE TABLE open_citation_parallels (
-  oci VARCHAR(1000),
-  citing VARCHAR(400),
-  cited VARCHAR(400),
-  creation_date DATE,
-  time_span INTERVAL,
-  journal_sc BOOLEAN,
-  author_sc BOOLEAN,
+CREATE TABLE open_citation_parallels
+(
+  oci             VARCHAR(1000),
+  citing          VARCHAR(400),
+  cited           VARCHAR(400),
+  creation_date   DATE,
+  time_span       INTERVAL,
+  journal_sc      BOOLEAN,
+  author_sc       BOOLEAN,
   citing_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date) ) STORED,
-  cited_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date - time_span) ) STORED,
+  cited_pub_year  SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date - time_span) ) STORED,
   CONSTRAINT open_citation_parallels_pk
     PRIMARY KEY (oci) USING INDEX TABLESPACE open_citations_tbs
 ) TABLESPACE open_citations_tbs;
@@ -93,16 +96,17 @@ COMMENT ON TABLE open_citation_parallels IS 'Citations that parallel (citing -> 
 ALTER TABLE open_citation_parallels
   OWNER TO devs;
 
-CREATE TABLE open_citation_self (
-  oci VARCHAR(1000),
-  citing VARCHAR(400),
-  cited VARCHAR(400),
-  creation_date DATE,
-  time_span INTERVAL,
-  journal_sc BOOLEAN,
-  author_sc BOOLEAN,
+CREATE TABLE open_citation_self
+(
+  oci             VARCHAR(1000),
+  citing          VARCHAR(400),
+  cited           VARCHAR(400),
+  creation_date   DATE,
+  time_span       INTERVAL,
+  journal_sc      BOOLEAN,
+  author_sc       BOOLEAN,
   citing_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date) ) STORED,
-  cited_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date - time_span) ) STORED,
+  cited_pub_year  SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date - time_span) ) STORED,
   CONSTRAINT open_citation_self_pk
     PRIMARY KEY (oci) USING INDEX TABLESPACE open_citations_tbs
 ) TABLESPACE open_citations_tbs;
@@ -112,16 +116,17 @@ COMMENT ON TABLE open_citation_self IS 'Citations with citing = cited';
 ALTER TABLE open_citation_self
   OWNER TO devs;
 
-CREATE TABLE open_citation_loops (
-  oci VARCHAR(1000),
-  citing VARCHAR(400),
-  cited VARCHAR(400),
-  creation_date DATE,
-  time_span INTERVAL,
-  journal_sc BOOLEAN,
-  author_sc BOOLEAN,
+CREATE TABLE open_citation_loops
+(
+  oci             VARCHAR(1000),
+  citing          VARCHAR(400),
+  cited           VARCHAR(400),
+  creation_date   DATE,
+  time_span       INTERVAL,
+  journal_sc      BOOLEAN,
+  author_sc       BOOLEAN,
   citing_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date) ) STORED,
-  cited_pub_year SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date - time_span) ) STORED,
+  cited_pub_year  SMALLINT GENERATED ALWAYS AS ( extract(YEAR FROM creation_date - time_span) ) STORED,
   CONSTRAINT open_citation_loops_pk
     PRIMARY KEY (oci) USING INDEX TABLESPACE open_citations_tbs
 ) TABLESPACE open_citations_tbs;
@@ -132,7 +137,14 @@ ALTER TABLE open_citation_loops
   OWNER TO devs;
 
 CREATE OR REPLACE VIEW stg_open_citations AS
-SELECT oci, citing, cited, 'foo' AS creation, 'bar' AS timespan, journal_sc, author_sc, 'baz' AS source
+SELECT oci,
+       citing,
+       cited,
+       'foo' AS creation,
+       'bar' AS timespan,
+       journal_sc,
+       author_sc,
+       'baz' AS source
 FROM open_citations;
 
 \include_relative trg_transform_and_load_open_citation.sql
@@ -144,14 +156,12 @@ CREATE MATERIALIZED VIEW open_citation_pubs
   --CREATE TABLE open_citation_pubs
   TABLESPACE open_citations_tbs AS
 SELECT sq.doi, nextval('open_citation_pubs_seq') AS iid
-FROM (
-       SELECT citing AS doi
-       FROM open_citations
-       UNION
-       -- DISTINCT values only
-       SELECT cited
-       FROM open_citations
-     ) sq
+FROM (SELECT citing AS doi
+      FROM open_citations
+      UNION
+      -- DISTINCT values only
+      SELECT cited
+      FROM open_citations) sq
 WITH NO DATA;
 
 COMMENT ON MATERIALIZED VIEW open_citation_pubs IS ---
@@ -173,6 +183,7 @@ ALTER MATERIALIZED VIEW open_citation_pubs
 
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO PUBLIC;
 GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO PUBLIC;
+GRANT pg_read_server_files TO devs;
 
 GRANT ALL ON ALL TABLES IN SCHEMA public TO jenkins;
 GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO jenkins;
