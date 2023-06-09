13e60be1c9145bbdaa8a3096fd46b2d46fc79071
Dima Korobskiy <dkroot2@gmail.com>
2023-06-08 21:57:34 -0400

    Add the DB model diagram

--- Postgres/DDL/master_postgres_ddl.sql
+++ Postgres/DDL/master_postgres_ddl.sql
@@ -2,7 +2,3 @@
 
 \include_relative running_view.sql
 \include_relative Functions/upsert_file.sql
-
-\include_relative ../../Scopus/Postgres/DDL/scopus_ddl.sql
-
-\include_relative ../../Trials/Postgres/DDL/ct_ddl.sql
