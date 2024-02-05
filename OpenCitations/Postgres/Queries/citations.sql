8e5ecf2c2b686172ed64f650367e9f4035a3a3f7
Dima Korobskiy <dkroot2@gmail.com>
2024-02-05 17:45:59 -0500

    ETL for OpenCitations Meta: add an index

--- OpenCitations/Postgres/Queries/citations.sql
+++ OpenCitations/Postgres/Queries/citations.sql
@@ -8,30 +8,56 @@ SET search_path = :schema;
 -- JetBrains IDEs: start execution from here
 SET TIMEZONE = 'US/Eastern';
 
+-- Find a pub by a pub's other id, e.g. `doi:10.1016/j.cell.2017.07.029`
+SELECT *
+FROM open_citation_pub_ids ocpi
+WHERE ocpi.id = '${other_id}';
+-- doi:10.1016/j.cell.2017.07.029: omid:br/0640681171 -- 0.1s
+
+-- Find # of cited works by a pub's other id, e.g. `doi:10.1016/j.cell.2017.07.029`
+SELECT count(1)
+FROM open_citation_pub_ids ocpi
+JOIN open_citations oc ON oc.citing = ocpi.omid
+WHERE ocpi.id = '${other_id}';
+-- doi:10.1016/j.cell.2017.07.029: 299 -- 0.1s
+
+-- Find # of inbound citations of the work by a pub's other id, e.g. `doi:10.1016/j.cell.2017.07.029`
+SELECT count(1)
+FROM open_citation_pub_ids ocpi
+JOIN open_citations oc ON oc.cited = ocpi.omid
+WHERE ocpi.id = '${other_id}';
+-- doi:10.1016/j.cell.2017.07.029: 1511 -- 0.1s
+
 --region Check for separation in `open_citations*` tables
 SELECT *
-FROM open_citation_loops
-WHERE oci IN (SELECT oci
-              FROM open_citations);
+FROM open_citations_looping
+WHERE oci IN (
+  SELECT oci
+  FROM open_citations
+);
 
 SELECT *
-FROM open_citation_parallels
-WHERE oci IN (SELECT oci
-              FROM open_citations);
+FROM open_citations_parallel
+WHERE oci IN (
+  SELECT oci
+  FROM open_citations
+);
 
 SELECT *
-FROM open_citation_self
-WHERE oci IN (SELECT oci
-              FROM open_citations);
+FROM open_citations_self
+WHERE oci IN (
+  SELECT oci
+  FROM open_citations
+);
 --endregion
 
 -- Remove total duplicates in `open_citation_duplicates`
 DELETE
-FROM open_citation_duplicates ocd
-WHERE exists (SELECT 1
-              FROM open_citations oc
-              WHERE (oc.oci, oc.citing, oc.cited, oc.creation_date, oc.time_span, oc.author_sc,
-                     oc.journal_sc) IS NOT DISTINCT FROM (ocd.oci, ocd.citing, ocd.cited, ocd.creation_date,
-                                                          ocd.time_span,
-                                                          ocd.author_sc,
-                                                          ocd.journal_sc));
+FROM open_citations_duplicate ocd
+WHERE exists (
+  SELECT 1
+  FROM open_citations oc
+  WHERE (oc.oci, oc.citing, oc.cited, oc.citing_pub_date, oc.time_span, oc.author_sc, oc.journal_sc)--
+    IS NOT DISTINCT FROM (ocd.oci, ocd.citing, ocd.cited, ocd.citing_pub_date, ocd.time_span, ocd.author_sc,
+                          ocd.journal_sc)
+);
